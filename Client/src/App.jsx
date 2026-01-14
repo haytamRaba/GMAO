23217534d0e42e708c2f2d77966b42ba4330a4b8
haytam <haytamraba11@gmail.com>
2026-01-14 01:16:51 +0100

    feat(frontend): initialize frontend architecture

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  
-
   return (
-    <>
-      <h1>GMAO</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
